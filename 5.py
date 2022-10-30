a78d67c8a9e4d2c7ff5543025adb67d17c237bf4
Andrey <megainvest@yandex.ru>
2022-10-30 19:01:56 +0300

    Решение всех возможных задач

--- 5.py
+++ 5.py
@@ -46,12 +46,41 @@
 # Сколько конфет нужно взять первому игроку, 
 # чтобы забрать все конфеты у своего конкурента?
 
-# a) Добавьте игру против бота
-# b) Подумайте как наделить бота ""интеллектом""
-# player_vs_player()
+# import random
 
-from random import *
-import os
+# name1 = input('Введите имя игрока 1: ')
+# name2 = input('Введите имя игрока 2: ')
+# igrok = random.randrange(1, 3)
+# a = int(input('Введите колличество конфет: '))
+# if igrok == 1:
+#     print('Первый ход делает', name1)
+# else:
+#     print('Первый ход делает', name2)
+# while a > 0:
+#     if a > 28:
+#         igrok1 = int(input('Ты можешь взять не больше 28 конфет, сколько конфет ты взял? '))
+#         a = a - igrok1
+#         if a > 28:
+#             igrok2 = int(input('Ты можешь взять не больше 28 конфет, сколько конфет ты взял? '))
+#             a = a - igrok2
+#             if a <= 28:
+#                 if igrok == 1:
+#                     print('Осталось', a, 'штук, их забирает', name1, 'а значит победил', name1)
+#                 else:
+#                     print('Осталось', a, 'штук, их забирает', name2, 'а значит победил', name2)
+#         else:
+#             if igrok == 1:
+#                 print('Осталось', a, 'штук, их забирает', name2, 'а значит победил', name2)
+#             else:
+#                 print('Осталось', a, 'штук, их забирает', name1, 'а значит победил', name1)
+
+  
+
+# # a) Добавьте игру против бота
+# # b) Подумайте как наделить бота ""интеллектом""
+
+# from random import *
+# import os
 
 # def player_vs_bot():
 #     candies_total = 2021
@@ -59,19 +88,18 @@ import os
 #     player_1 = input('\nКак тебя зовут?: ')
 #     player_2 = 'Компьютер'
 #     players = [player_1, player_2]
-#     print(f'\nНу чтож {player_1} и  {player_2} да начнется игра !\n')
-#     print('\nДля начала опеределим кто первый начнет игру.\n')
+#     print(f'\nИграют {player_1} и  {player_2}.\n')
+#     print('\nОпеределим кто первый начнет игру.\n')
 
 #     lucky = randint(-1, 0)
 
-#     print(f'Поздравляю {players[lucky+1]} ты ходишь первым !')
+#     print(f'{players[lucky+1]} ты ходишь первым !')
 
 #     while candies_total > 0:
 #         lucky += 1
 
 #         if players[lucky % 2] == 'Компьютер':
-#             print(
-#                 f'\nХодит {players[lucky%2]} \nНа кону {candies_total}. \n: ')
+#             print(f'\nХодит {players[lucky%2]}. \n: ')
 
 #             if candies_total < 29:
 #                 step = candies_total
@@ -98,3 +126,49 @@ import os
 
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+# with open('file_encode.txt', 'w') as data:
+#     data.write('wwwwwwwwwwwwwwdddddddddddaaaaaaaaaaaffgggggggggghhhhhhhhh')
+
+# with open('file_encode.txt', 'r') as data:
+#     string = data.readline()
+
+# def rle_encode(decoded_string):
+#     encoded_string = ''
+#     count = 1
+#     char = decoded_string[0]
+#     for i in range(1, len(decoded_string)):
+#         if decoded_string[i] == char:
+#             count += 1
+#         else:
+#             encoded_string = encoded_string + str(count) + char
+#             char = decoded_string[i]
+#             count = 1
+#             encoded_string = encoded_string + str(count) + char
+#     return encoded_string
+
+
+# def rle_decode(encoded_string):
+#     decoded_string = ''
+#     char_amount = ''
+#     for i in range(len(encoded_string)):
+#         if encoded_string[i].isdigit():
+#             char_amount += encoded_string[i]
+#         else:
+#             decoded_string += encoded_string[i] * int(char_amount)
+#         char_amount = ''
+#     print(decoded_string)
+
+#     return decoded_string
+
+
+# with open('file_encode.txt', 'r') as file:
+#     decoded_string = file.read()
+
+# with open('file_decode.txt', 'w') as file:
+#     encoded_string = rle_encode(decoded_string)
+#     file.write(encoded_string)
+
+# print('Decoded string: \t' + decoded_string)
+# print('Encoded string: \t' + rle_encode(decoded_string))
+# print(f'Compress ratio: \t{round(len(decoded_string) / len(encoded_string), 1)}')
